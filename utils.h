111b78f3bc5ba9f33a5fb4872b67d519ef294eb5
alexxiordache <iordachealex2006@gmail.com>
2026-06-30 11:50:01 +0300

    Button and BoardSquare classes

--- utils.h
+++ utils.h
@@ -7,6 +7,57 @@ const float WINDOW_W = 1200.0f;
 const float WINDOW_H = 1200.0f;
 const float PADDING = 10.0f;
 const float SQUARE_SIZE = 100.0f;
+using namespace std;
+
+class ButtonData {
+    protected:
+        sf::RectangleShape shape;
+        string name;
+        bool pressed = false;
+    public:
+        void setShape(sf::RectangleShape &s) {
+            shape = s;
+        }
+        void setName(std::string s) {
+            name = s;
+        }
+        void toggle() {
+            pressed ^= 1;
+        }
+        sf::RectangleShape getShape() const {
+            return shape;
+        }
+        string GetName() const {
+            return name;
+        }
+        bool isPressed() const {
+            return pressed;
+        }
+        bool Update(const sf::Vector2f& mousePos) {
+            bool actionTriggered = false; 
+            if (shape.getGlobalBounds().contains(mousePos)) 
+                if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+                    if (!isPressed()) 
+                        actionTriggered = true;  
+                    toggle();
+                }
+            return actionTriggered; 
+        }
+        virtual ~ButtonData() {};
+};
+
+class BoardSquare: public ButtonData {
+    private:
+        bool color; // 0 - white, 1 - black
+        // Piece piece;
+    public:
+        void init(sf::RectangleShape s, bool c) {
+            shape = s; 
+            color = c;
+        }
+};
+
+BoardSquare board[8][8];
 
 sf::RectangleShape DrawRectangle(sf::RenderWindow& window, float size_x, float size_y, float pos_x, float pos_y, 
     sf::Color color, float outline_thickness = 0.0f, sf::Color outline_color = sf::Color::Transparent) {
@@ -28,9 +79,9 @@ void DrawBoard(sf::RenderWindow& window) {
         current_color ^= 1;
         for(int j = 0; j < 8; j++) {
             if(!current_color)
-                DrawRectangle(window, SQUARE_SIZE, SQUARE_SIZE, x, y, sf::Color::White);
+                board[i][j].init(DrawRectangle(window, SQUARE_SIZE, SQUARE_SIZE, x, y, sf::Color::White), false);
             else 
-                DrawRectangle(window, SQUARE_SIZE, SQUARE_SIZE, x, y, sf::Color::Black);
+                board[i][j].init(DrawRectangle(window, SQUARE_SIZE, SQUARE_SIZE, x, y, sf::Color::Black), true);
             current_color ^= 1;
             x += SQUARE_SIZE;
         }
